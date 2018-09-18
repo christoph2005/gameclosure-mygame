8a8b5c3831d02f996973c1fe2387f3b9ee9dce4c
christoph2005 <christoph2005@gmail.com>
2018-09-18 11:24:23 -0700

    k, I added a "Gem Actor" and was able to figure out how to keep it from rendering uneccessary ImageViews, without just setting opacity.

--- mygame/src/Gem.js
+++ mygame/src/Gem.js
@@ -27,29 +27,38 @@ exports = Class(ui.View, function (supr) {
 	 * Layout
 	 */
 	this.build = function () {
-      var w = blue_img.getWidth(),
-          h = blue_img.getHeight(),
-          sx = 0-w,
-          sy = 0-h;
-		var blue = new ui.ImageView({
-			superview: this,
-			image: blue_img,
-			x: sx,
-			y: sy,
-         scale:1,
-			width: w,
-			height: h
-		});
-		var green = new ui.ImageView({
-			superview: this,
-			image: green_img,
-			x: sx,
-			y: sy,
-         scale:1,
-			width: w,
-			height: h
-		});
-      console.log(green);
+      var that = this;
+      function gem(color){
+         var w = blue_img.getWidth(),
+             h = blue_img.getHeight(),
+             sx = 0-w,  
+             sy = 0-h;
+             var img;
+             if (color == 'blue'){
+               img = blue_img;
+             } else if (color == 'green'){
+               img = green_img;
+             } else if (color == 'purple'){
+               img = purple_img;
+             } else if (color == 'red'){
+               img = red_img;
+             } else {
+               img = yellow_img;
+             } 
+         return new ui.ImageView({
+            superview: this.view,
+            image: img,
+            x: 0-sx,
+            y: 600-sy,
+            scale:1,
+            width: w,
+            height: h
+         });
+      }
+		var blue = new gem('blue');
+      this.addSubview(blue);
+      blue.style.x = blue.style.x+500;
+      console.log(blue);
 		//var sound = soundcontroller.getSound();
 
 	};
